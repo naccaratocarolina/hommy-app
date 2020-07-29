2f0faea2c3bb9dad1f12b294fdba8c6911e57636
Carolina Naccarato <naccaratocarolina@gmail.com>
2020-07-29 13:21:40 -0300

    + RepublicTableSeeder

--- back-end/database/seeds/UserTableSeeder.php
+++ back-end/database/seeds/UserTableSeeder.php
@@ -10,13 +10,6 @@ class UserTableSeeder extends Seeder
      * @return void
      */
     public function run() {
-        factory (App\User::class, 12)->create()->each(function ($user) {
-          $comments = factory(App\Comment::class, 2)->make();
-          $republic = factory(App\Republic::class)->make();
-          //relacao 1-n
-          $user->comments()->saveMany($comments);
-          //relacao 1-1
-          $user->republic()->save($republic);
-        });
+        factory (App\User::class, 12)->create();
     }
 }
