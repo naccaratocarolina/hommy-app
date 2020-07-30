83d9a7e21243178a20a4d3a268cf5f857bf18998
Carolina Naccarato <naccaratocarolina@gmail.com>
2020-07-30 00:49:47 -0300

    + PassportController ok

--- back-end/app/Http/Controllers/API/PassportController.php
+++ back-end/app/Http/Controllers/API/PassportController.php
@@ -4,34 +4,59 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Requests\UserRequest as UserRequest;
+use Illuminate\Support\Facades\Validator;
 
 use App\User;
-use App\Http\Requests\UserRequest;
 use Auth;
 use DB;
 
 class PassportController extends Controller {
+    public $successStatus = 200;
+
     public function register(UserRequest $request) {
       $new_user = new User;
-      $new_user->createUser($request);
-      $success['token'] = $new_user->createToken('MyApp')->accessToken;
-      return response()->json(['success' => $success, 'user' => $new_user], 200);
+      $new_user->name = $request->name;
+      $new_user->email = $request->email;
+      $new_user->password = bcrypt($request->password);
+      $new_user->save();
+      $token = $new_user->createToken('MyApp')->accessToken;
+      $name = $new_user->name;
+      return response()->json([
+            "message" => "cadastro realizado com sucesso!",
+            "data" => [
+            'user' => $new_user,
+            'token' =>$token
+            ]
+      ], 200);
     }
 
     public function login() {
-      if(Auth::attempt(['email' => request('email'), 'password' => request('password')])) {
+      if (Auth::attempt(['email' => request('email'), 'password' => request('password')])) {
         $user = Auth::user();
-        $success['token'] = $user->createToken('MyApp')->accessToken;
-        return response()->json(['success' => $success], 200);
+        $token = $user->createToken('MyApp')->accessToken;
+        $name = $user->name;
+        return response()->json([
+          "message" => "login realizado com sucesso!",
+          "data" => [
+            'user' => $user,
+            'token' => $token
+            ]
+          ], 200);
       }
       else {
-        return response()->json(['error' => 'Unauthorized', 'status' => 401]);
-      }
+        return response()->json([
+          "message" => "email e senha invalidos!",
+          "data" => [
+            null
+            ]
+          ], 500);
+        }
     }
 
     public function getDetails() {
       $user = Auth::user();
-      return response()->json(['user' => $user], 200);
+      return response()->json(['user' => $user,], 200);
     }
 
     public function logout() {
