b9bc3d373265b2a37c4c99acc8ff0e0bdec56212
naccaratocarolina <naccaratocarolina@gmail.com>
2020-07-31 13:14:33 -0300

    INTEGRACAO OK AAAAAA

--- front-end/src/app/home2/home2.page.ts
+++ front-end/src/app/home2/home2.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SearchService } from '../services/search.service';
 
 @Component({
   selector: 'app-home2',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home2.page.scss'],
 })
 export class Home2Page implements OnInit {
+  public republicsArray = [];
 
-  constructor() { }
+  constructor(public searchService: SearchService) { }
 
   ngOnInit() {
+    this.listAllRepublics();
   }
 
+  listAllRepublics() {
+    this.searchService.getListRepublic().subscribe((res) => {
+      this.republicsArray = res;
+      console.log(this.republicsArray);
+    });
+  }
 }
