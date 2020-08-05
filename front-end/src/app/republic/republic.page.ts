291614ffafc8471fe7f3c46e821d9f017a00ec68
naccaratocarolina <naccaratocarolina@gmail.com>
2020-08-05 00:38:28 -0300

    ajeitando os comentarios

--- front-end/src/app/republic/republic.page.ts
+++ front-end/src/app/republic/republic.page.ts
@@ -10,28 +10,26 @@ import { CommentService } from '../services/comment.service';
   styleUrls: ['./republic.page.scss'],
 })
 export class RepublicPage implements OnInit {
-  //array de comments
-  commentsArray = [];
-  //var que vai armazenar o republic_id do json do localStorage
-  republic_id:number;
-  //variaveis form builder
+  republic_id:number; //var que vai armazenar o republic_id do json do localStorage
+  //variaveis do listComment
+  commentsArray = []; //array de comments
+  //variaveis do addComment
   comment_form: FormGroup;
-  comment_edit_form: FormGroup;
-  //objetos do json retornado da funcao listComments do back-end
+  //variaveis do commentByRepublic
   republic:any; //armazena a rep dona do comment
   comments = []; //armazena um array de comments referente a republica
-  //nova variavel de texto para editar um comentario
-  text_edit:string = '';
-  //var que vai armazenar o id do comment
-  comment_id:number;
-  //var de controle do form
-  canEdit:boolean = false;
+  //variaveis do updateComment
+  comment_edit_form: FormGroup;
+  //variaveis do getCommentId
+  comment_id:number; //var que vai armazenar o id do comment
+  text_edit:string = ''; //nova variavel de texto para editar um comentario
+  canEdit:boolean = false; //var de controle do form
 
   constructor( public commentService: CommentService, public formbuilder: FormBuilder ) {
     //pega o republic_id do localStorage
     this.republic_id = JSON.parse(localStorage.getItem('republic')).id;
-    
-    //inicializa os forms
+
+    //inicializa os forms e valida os inputs
     this.comment_form = this.formbuilder.group({
       text: ['', [Validators.required]]
     	});
@@ -47,7 +45,7 @@ export class RepublicPage implements OnInit {
 
   listComment() {
     this.commentService.getListComment().subscribe((res) => {
-      this.commentsArray = res[0];
+      this.commentsArray = res[0]; //res[0] pq o back ta retornando um objeto de objetos
       console.log(this.commentsArray);
     });
   }
@@ -67,9 +65,9 @@ export class RepublicPage implements OnInit {
   commentByRepublic(republic_id) {
     this.commentService.getCommentByRepublic(republic_id).subscribe((res) => {
       console.log(res);
-      this.republic = res.republic; //printa a republica dona do comentario
+      this.republic = res.republic; //pega a republica dona do comentario
       console.log(this.republic);
-      this.comments = res.comments; //printa um array de comentarios da republica
+      this.comments = res.comments; //pega um array de comentarios da republica
       console.log(this.comments);
     });
   }
@@ -91,7 +89,7 @@ export class RepublicPage implements OnInit {
     this.comment_id = id;
     console.log(this.comment_id);
     for(let comment of this.comments) {
-      if(comment.id == id) { //seleciona o comment com o id dado
+      if(comment.id === id) { //seleciona o comment com o id dado
         this.text_edit = comment.text;
       }
     }
